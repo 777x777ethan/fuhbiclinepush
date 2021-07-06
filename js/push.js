8ef5a1d05d37f3e62e0cbd742631e265413ee282
777x777ethan <39490292+777x777ethan@users.noreply.github.com>
2021-07-06 12:25:34 +0800

    Add files via upload

---
+++ js/push.js
@@ -0,0 +1,11 @@
+function LinePush() {
+    var userID = document.getElementById("userID").value;
+    var txtID = document.getElementById("txtID").value;
+    if (userID != '' && txtID != '') {
+        var xhr = new XMLHttpRequest();
+        var _url = "https://script.google.com/macros/s/AKfycbxsOmuc3Z34fy5WXQ1QGeLEM89ukn8dtK7wQt82Y4nd4ptfjAyyPB262cjD48Yx3NGHmg/exec";
+        xhr.open("POST", _url, true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send("UserID=" + userID + "&TxtID=" + txtID);
+    }
+}

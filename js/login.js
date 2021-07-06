fef004136f840ee40d20b89b63e7214d2e33db5f
777x777ethan <39490292+777x777ethan@users.noreply.github.com>
2021-07-06 10:54:10 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,7 +2,16 @@ var urlData = location.search.trimStart("?");
 var _code = urlData.split("&")[0].split("=")[1];
 var _name = urlData.split("&")[1].split("=")[1];
 if (_code != '' && _name != '') {
-    console.log(_code);
+    var xhr = new XMLHttpRequest();
+    var _url = "https://script.google.com/macros/s/AKfycbycRVO2_r8E1KW5ZVieR4ekC4IdNg6xajgJHshJy7B2JYM0kZ2dMPXqj76cn-dC0rmlTQ/exec";
+    xhr.open("POST", _url, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send("UserName=" + _name + "&UserID=" + _code);
+    xhr.onload = function() {
+        if (this.responseText = "OK") {
+            console.log("OK")
+        }
+    }
 }
 
 function Login() {
@@ -16,15 +25,6 @@ function Login() {
         _changeUrl += "&state=" + userID;
         window.location.assign(_changeUrl);
 
-        // var xhr = new XMLHttpRequest();
-        // var _url = "https://script.google.com/macros/s/AKfycbycRVO2_r8E1KW5ZVieR4ekC4IdNg6xajgJHshJy7B2JYM0kZ2dMPXqj76cn-dC0rmlTQ/exec";
-        // xhr.open("POST", _url, true);
-        // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        // xhr.send("UserName=" + userID);
-        // xhr.onload = function() {
-        //     if (this.responseText = "OK") {
 
-        //     }
-        // }
     }
 }

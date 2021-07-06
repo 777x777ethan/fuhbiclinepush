213ce21ec4ce1402b09ddfd87009c01113c52576
777x777ethan <39490292+777x777ethan@users.noreply.github.com>
2021-07-06 10:49:17 +0800

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,21 +1,30 @@
+var urlData = location.search.trimStart("?");
+var _code = urlData.split("&")[0].split("=")[1];
+var _name = urlData.split("&")[1].split("=")[1];
+if (_code != '' && _name != '') {
+    console.log(_code);
+}
+
 function Login() {
     var userID = document.getElementById("userID").value;
     if (userID != '') {
-        var xhr = new XMLHttpRequest();
-        var _url = "https://script.google.com/macros/s/AKfycbycRVO2_r8E1KW5ZVieR4ekC4IdNg6xajgJHshJy7B2JYM0kZ2dMPXqj76cn-dC0rmlTQ/exec";
-        xhr.open("POST", _url, true);
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        xhr.send("UserName=" + userID);
-        xhr.onload = function() {
-            if (this.responseText = "OK") {
-                var _changeUrl = "https://notify-bot.line.me/oauth/authorize?";
-                _changeUrl += "response_type=code";
-                _changeUrl += "&client_id=7flF9g8ZjNmsHjXz2UV8HE";
-                _changeUrl += "&redirect_uri=http://localhost/EthanLinePush";
-                _changeUrl += "&scope=notify";
-                _changeUrl += "&state=" + userID;
-                window.location.assign(_changeUrl);
-            }
-        }
+        var _changeUrl = "https://notify-bot.line.me/oauth/authorize?";
+        _changeUrl += "response_type=code";
+        _changeUrl += "&client_id=7flF9g8ZjNmsHjXz2UV8HE";
+        _changeUrl += "&redirect_uri=https://777x777ethan.github.io/fuhbiclinepush/";
+        _changeUrl += "&scope=notify";
+        _changeUrl += "&state=" + userID;
+        window.location.assign(_changeUrl);
+
+        // var xhr = new XMLHttpRequest();
+        // var _url = "https://script.google.com/macros/s/AKfycbycRVO2_r8E1KW5ZVieR4ekC4IdNg6xajgJHshJy7B2JYM0kZ2dMPXqj76cn-dC0rmlTQ/exec";
+        // xhr.open("POST", _url, true);
+        // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        // xhr.send("UserName=" + userID);
+        // xhr.onload = function() {
+        //     if (this.responseText = "OK") {
+
+        //     }
+        // }
     }
 }
